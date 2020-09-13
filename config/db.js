b72a274272909e014257c290ba51450f7695e9d4
Arun Kumar <java.kumar.arun@gmail.com>
2020-09-13 21:43:05 +0530

    db config done

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const Sequelize = require('sequelize');
 
 let sequelize = new Sequelize('petclinic', 'petclinic', 'password', {
   host: 'localhost', port: 3306,
-  host: '192.168.99.100', port: 3307,
+  // host: '192.168.99.100', port: 3307,
 
 
 // let sequelize = new Sequelize('notesapp', 'root', 'abcdef', {
